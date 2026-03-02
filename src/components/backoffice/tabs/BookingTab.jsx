b393b5a8efc4eeb38ef2cc5c918e84c3cf2c6782
Vincenza <vinni2690@hotmail.com>
2026-03-02 11:02:11 +0100

    bookings fetch

--- src/components/backoffice/tabs/BookingTab.jsx
+++ src/components/backoffice/tabs/BookingTab.jsx
@@ -1,9 +1,39 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
 
 
 const BookingTab = function () {
 
+    const APIUrlGetBookings = 'http://localhost:8080/bookings'
+    const token = localStorage.getItem("token")
+
+    const [bookings, setBookings] = useState([])
+
+    const getBookings = () => {
+        axios
+        .get(APIUrlGetBookings, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+                "Content-Type": "application/json",
+            }
+        })
+        .then((response) => {
+            console.log(response.data.content, 'bookings')
+            
+            setBookings(response.data.content)
+        })
+        .catch((error) => {
+            console.log("errore nel recupero menu", error)
+            setIsLoading(false)
+            setIsError(true)
+            
+        })
+    }
 
 
+    useEffect(()=>{
+            getBookings()
+        }, [])
 
     return (
         <section className="">
