037b71e10532547fa0c994a712133d1a504f3e43
Vincenza <vinni2690@hotmail.com>
2026-03-12 09:51:04 +0100

    dishes responsive

--- src/components/backoffice/tabs/DishTab.jsx
+++ src/components/backoffice/tabs/DishTab.jsx
@@ -40,15 +40,15 @@ const DishTab = function () {
         }, [])
 
     return (
-        <section className="p-10">
+        <section className="p-3 md:p-10">
             <div className="flex flex-col md:flex-row flex-wrap gap-5 items-center justify-center">
 
             {dishes.map((dish) => (
 
-                <div key={dish.id} className="border-2 border-slate-500 py-5 rounded-2xl text-center text-2x w-xs">
+                <div key={dish.id} className="border-2 border-slate-500 py-5 rounded-2xl text-center text-2x md:w-xs">
                     <h4 className="text-3xl capitalize mb-2">{dish.name}</h4>
                     <h5 className="font-semibold mb-2 text-2xl">{dish.title}</h5>
-                    <img src={dish.image} alt={dish.title} className="w-sm h-48 object-cover" />
+                    <img src={dish.image} alt={dish.title} className=" md:w-sm md:h-48 object-cover" />
                     <p className="text-lg p-5">{dish.description}</p>
                     <button to={`/reservar`} className=" text-white  hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-teal-600 hover:bg-primary-700 focus:ring-primary-800 hover:bg-teal-500">Modifica</button>
                 </div>
