d65b2dd05d8e356e5cb26e7f473b68b989f67bd2
Vincenza <vinni2690@hotmail.com>
2026-06-30 10:50:55 +0200

    map added

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -48,6 +48,25 @@ const HomePage = function () {
 
             </div>
 
+            <div className="bg-black p-20 items-center">
+                <p>spacing</p>
+            </div>
+
+            {/* find us */}
+
+            <div className="flex flex-col items-center gap-8 p-6 text-black dark:text-white bg-white dark:bg-black">
+                <h3 className="text-3xl text-shadow-md/20">Donde estamos</h3>
+                
+                <div className="flex flex-col md:flex-row m:w-xl">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5790.225290108554!2d10.32744894740432!3d43.47911951957426!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12d5e61e2519c089%3A0x70bfd41bcbc381dd!2sCalafuria%20Province%20of%20Livorno%2C%20Italy!5e0!3m2!1sen!2ses!4v1752496589592!5m2!1sen!2ses" width="600" height="450" loading="lazy" className="w-full"></iframe>
+                    {/* <img src="restaurant-interior.jpg" alt="photo of the interior of the restaurant" /> */}
+                    
+                </div>
+
+                
+
+            </div>
+
             <div className="flex flex-row items-center justify-center p-5 text-black dark:text-white bg-white dark:bg-black">
                 {/* <div className="flex flex-col items-center">
                     <h3>Dirección</h3>
@@ -66,6 +85,7 @@ const HomePage = function () {
                         <Link to={'https://www.instagram.com/'} target="_blank" className=" underline text-white hover:text-red-600"><img src="insta-logo.png" alt="" /></Link>
                         <Link to={'https://www.facebook.com/'} target="_blank" className=" underline text-white hover:text-red-600"><img src="fb-logo.png" alt="" /></Link>
                         <Link to={'/https://x.com/'} target="_blank" className=" underline text-white hover:text-red-600"><img src="x-logo.png" alt="" /></Link>
+                        
                     </div>
                 </div>
             </div>
